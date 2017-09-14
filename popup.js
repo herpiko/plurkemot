56fef85b4f31d6b68e3d81956f5c02d0a8423ef0
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-09-14 13:07:17 +0700

    Update the changed key prefix.

--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ function loadEmots(cb){
     var keys = Object.keys(emots);
     if (keys.length > 0) {
       keys.forEach(function(key){
-        if (key.indexOf('plurkemotURL') > -1 && emots[key].indexOf('https://emos.plurk.com') > -1) {
+        if (key.indexOf('plurkemot') > -1 && emots[key].indexOf('https://emos.plurk.com') > -1) {
           emotsDiv.innerHTML += '<img class="emot" src="' + emots[key] + '">';
         }
       });
@@ -23,28 +23,6 @@ function loadEmots(cb){
     } 
   });
 }
-
-function handleFileSelect(evt) {
-  var file = evt.target.files[0]; // FileList object
-  var reader = new FileReader();
-  reader.onload = function(evt) {
-    // TODO : clear the evt
-    var id = (new Date()).valueOf();
-    var key = 'plurkemotB64' + id.toString();
-    var obj = {};
-    obj[key] = evt.target.result; 
-    chrome.storage.sync.set(obj, function(){
-      chrome.storage.sync.get(key, function(item){
-        // TODO push to plurk API
-        loadEmots();
-      });
-    });
-  }
-  reader.readAsDataURL(file, 'utf8');
-}
-
-
-//document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 setTimeout(function(){
   loadEmots(function(){
