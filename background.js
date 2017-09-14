b305dd71d3e50be721549c70cb01a54cbfc812c5
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-09-14 12:43:03 +0700

    Check against existing emots.

--- background.js
+++ background.js
@@ -2,12 +2,24 @@ chrome.contextMenus.create({
   title: "Save emoticon",
   contexts : ["image"],
   onclick : function(content) {
-    var id = (new Date()).valueOf();
-    var key = "plurkemotURL" + id.toString();
-    var obj = {};
-    obj[key] = content.srcUrl; 
-    chrome.storage.sync.set(obj, function(){
-      console.log('Emot saved successfuly (dance)');
-    });
+    if (content.srcUrl.indexOf('https://emos.plurk.com') < 0) {
+      return alert('Failed to save. The emot should be a custom plurk emot from emos.plurk.com');
+    }
+    try {
+      var key = "plurkemot_emosplurk_" + content.srcUrl.split('com/')[1].split('.')[0];
+      var obj = {};
+      obj[key] = content.srcUrl; 
+      chrome.storage.sync.get(key, function(existing){
+        if (Object.keys(existing).length > 0) {
+          return alert('The emot is already exists');
+        }
+        chrome.storage.sync.set(obj, function(){
+          alert('Emot saved successfuly (dance)');
+        });
+      });
+    } catch(e) {
+      console.log(e);
+      alert('Failed to save the emot.');
+    }
   }
 });
